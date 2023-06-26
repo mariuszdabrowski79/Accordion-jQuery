e43b8038d6008ede3208bc898e5232f7fd0eff3e
mariuszdabrowski79 <mariuszdabrowski79@op.pl>
2023-06-26 17:23:14 +0200

    Zadanie

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $('.accordion-item').click(function () {
+
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).find('.accordion-content').slideUp();
+        } else {
+            $(this).addClass('open');
+            $(this).find('.accordion-content').slideDown();
+        }
+
+    });
+
+});
+
